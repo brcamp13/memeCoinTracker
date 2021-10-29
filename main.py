736b86cceac31d429dc13cfa3ec05a47e96aadad
brcamp13 <brandon.camp.bell@outlook.com>
2021-10-28 21:47:52 -0500

    Add basic structure

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+def getMemeCoinsFromAPI():
+    return ['coin1', 'coin2']
+
+def createMemeCoinsFile(memeCoins):
+    return 'file created'
+
+def checkIfMemeCoinsFolderExists():
+    return False
+
+def createMemeCoinsFolder():
+    return True
+
+def addMemeCoinsFileToFolder(memeCoinsFile):
+    return True
+
+if __name__ == "__main__":
+    # Query api w/ list of meme coin strings
+    # Create a new file called potentialMemeCoin_s<DATE>_<TIME HH:MM:SS>
+    # Check if meme coin folder exists locally, if not, create it, otherwise add new file to it
+    # Print out the meme coin findings as well & inform user of file name that they've been saved to
