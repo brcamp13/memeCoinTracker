5328b3cbb7233f649522d6e30ced336cf55254b3
brcamp13 <brandon.camp.bell@outlook.com>
2021-10-29 22:03:52 -0500

    Get files to work

--- main.py
+++ main.py
@@ -28,16 +28,12 @@ def get_meme_coins():
     
 
 def create_meme_coins_file(meme_coins):
-    datetime_file_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    file_name = os.getcwd().join('meme_coins ' + datetime_file_string)
+    datetime_file_string = datetime.now().strftime("%d_%m_%Y %H_%M_%S")
+    file_name = os.getcwd() + '/memeCoins/meme_coins ' + datetime_file_string + '.txt'
 
-    os.chdir("memeCoins")
-
-    with open(file_name, 'w') as f:
+    with open(file_name, 'w+') as f:
         for meme_coin in sorted(meme_coins):
             f.write("%s\n" % meme_coin)
-
-    os.chdir('../')
 
     return file_name
 
@@ -52,14 +48,8 @@ def add_meme_coins_file_to_folder(meme_coins_file):
     return True
 
 if __name__ == "__main__":
-    # Check if meme coin folder exists locally, if not, create it
     if not check_if_meme_coins_folder_exists():
         create_meme_coins_folder()
-    # Query api w/ list of meme coin strings
     meme_coins = get_meme_coins()
-    # Create a new file called potentialMemeCoin_s<DATE>_<TIME HH:MM:SS>
     meme_coins_file_name = create_meme_coins_file(meme_coins)
-    # Print out the meme coin findings as well & inform user of file name that they've been saved to
-    print(meme_coins)
-    print(str(len(meme_coins)) + ' coins returned')
     print('File with coins can be found in ' + os.getcwd() + '/' + meme_coins_file_name)
